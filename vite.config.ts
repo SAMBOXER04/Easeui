93008d7896b756ba88c3602e37c9f536d446b56e
SAMBOXER04 <ankitsingh89480@gmail.com>
2026-08-15 14:51:42 +0530

    setup demo website

--- vite.config.ts
+++ vite.config.ts
@@ -17,23 +17,7 @@ export default defineConfig({
   },
 
   build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "EaseUI",
-      fileName: (format) => `easeui.${format}.js`,
-    },
-
-    cssCodeSplit: true,
-
-    rollupOptions: {
-      external: ["react", "react-dom"],
-
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
+    outDir: "dist",
+    emptyOutDir: true,
   },
 });
